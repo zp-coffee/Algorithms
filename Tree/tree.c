ef98595e99c0319f36045cf0d8962a8d66bebfea
coffee_mac <2062233909@qq.com>
2024-05-08 19:26:58 +0800

    2023.5.8

--- Tree/tree.c
+++ Tree/tree.c
@@ -108,6 +108,89 @@ void AddRightNode(TreeNode* p, int layer, int number, int target)
     cur->rnext = Lnode;
 }
 
+//创建一个线索树的根节点
+ThreadTreeNode* CreateRootThreadTree(void)
+{
+    ThreadTreeNode* root = (ThreadTreeNode *)malloc(sizeof(ThreadTreeNode));
+    root->rnext = NULL;
+    root->lnext = NULL;
+    return root;
+}
+
+//在树的第layer层的第number个位置添加一个节点作为左孩子，如果双亲不存在就返回错误
+void AddThreadLeftNode(ThreadTreeNode* p, int layer, int number, int target)
+{
+    ThreadTreeNode* Lnode = (ThreadTreeNode *)malloc(sizeof(ThreadTreeNode)); //先将节点创建好
+    Lnode->rnext = NULL;
+    Lnode->lnext = NULL;
+    Lnode->val = target;
+
+    ThreadTreeNode* cur = p;
+    for (int i = 1; i < layer-1; i ++)
+    {
+        if (number <= CalAccumulation(2, layer-i)/2) //在当前节点的左子树当中
+        {
+            if (cur->lnext == NULL)
+            {
+                printf("error\n");
+            }
+            else
+            {
+                cur = cur->lnext;
+            }
+        }
+        else                      //在当前节点的右子树中
+        {
+            if (cur->rnext == NULL)
+            {
+                printf("error\n");
+            }
+            else
+            {
+                cur = cur->rnext;
+            }
+        }
+    } //循环结束即找到了需要添加孩子的节点
+    cur->lnext = Lnode;
+}
+
+//在树的第几层的第几个节点添加右孩子,layer表示层数，number表示第几个节点，如果节点不存在就返回错误
+void AddThreadRightNode(ThreadTreeNode* p, int layer, int number, int target)
+{
+    ThreadTreeNode* Lnode = (ThreadTreeNode *)malloc(sizeof(ThreadTreeNode)); //先将节点创建好
+    Lnode->rnext = NULL;
+    Lnode->lnext = NULL;
+    Lnode->val = target;
+
+    ThreadTreeNode* cur = p;
+    for (int i = 1; i < layer-1; i ++)
+    {
+        if (number <= CalAccumulation(2, layer-i)/2) //在当前节点的左子树当中
+        {
+            if (cur->lnext == NULL)
+            {
+                printf("error\n");
+            }
+            else
+            {
+                cur = cur->lnext;
+            }
+        }
+        else                      //在当前节点的右子树中
+        {
+            if (cur->rnext == NULL)
+            {
+                printf("error\n");
+            }
+            else
+            {
+                cur = cur->rnext;
+            }
+        }
+    } //循环结束即找到了需要添加孩子的节点
+    cur->rnext = Lnode;
+}
+
 //计算一个值的几次方
 int CalAccumulation(int target, int num)
 {
@@ -175,3 +258,144 @@ int* Traversal(TreeNode* root, int* returnSize, int way)
     return ret;
 }
 
+ThreadTreeNode* pre = NULL;
+//中序遍历线索化树
+void ThreadinOrder(ThreadTreeNode* root)
+{
+    if (root != NULL)
+    {
+        ThreadinOrder(root->lnext);
+        ThreadVisit(root);
+        ThreadinOrder(root->rnext);
+    }
+}
+
+//在访问该节点时修改左指针和右指针为前继节点和后继节点
+void ThreadVisit(ThreadTreeNode* p)
+{
+    if (p->lnext == NULL)
+    {
+        p->lnext = pre;
+        p->ltag = 1;
+    }
+    if (pre != NULL && pre->rnext == NULL)
+    {
+        pre->rnext = p;
+        pre->rtag = 1;
+    }
+    pre = p;
+}
+
+//中序遍历线索化树的主函数
+void CreateInOrderThreadTree(ThreadTreeNode* root)
+{
+    pre = NULL;
+    if (root != NULL)
+    {
+        ThreadinOrder(root);
+        pre->rnext = NULL;
+        pre->rtag = 1;
+    }
+}
+
+//中序遍历线索化树的递归写法主函数
+void CreateInOrderThreadTree1(ThreadTreeNode* root)
+{
+    ThreadTreeNode* pre1 = NULL;
+    if (root != NULL)
+    {
+        ThreadinOrder1(root, pre1);
+        pre1->rnext = NULL;
+        pre1->rtag = 1;
+    }
+}
+
+//中序遍历线索化树的递归写法
+void ThreadinOrder1(ThreadTreeNode* p, ThreadTreeNode* pre1)
+{
+    if (p != NULL)
+    {
+        ThreadinOrder1(p->lnext, pre1);
+        if (p->lnext == NULL)
+        {
+            p->lnext = pre1;
+            p->ltag = 1;
+        }
+        if (pre1 != NULL && pre1->rnext == NULL)
+        {
+            pre1->rnext = p;
+            pre1->rtag = 1;
+        }
+        pre1 = p;
+        ThreadinOrder1(p->rnext, pre1);
+    }
+}
+
+//前序遍历线索化树
+void CreatePreOrderThreadTree(ThreadTreeNode* root)
+{
+    ThreadTreeNode* pre1 = NULL;
+    if (root != NULL)
+    {
+        ThreadpreOrder(root, pre1);
+        pre1->rnext = NULL;
+        pre1->rtag = 1;
+    }
+}
+
+//前序遍历线索化树的递归写法
+void ThreadpreOrder(ThreadTreeNode* p, ThreadTreeNode* pre1)
+{
+    if (p != NULL)
+    {
+        if (p->lnext == NULL)
+        {
+            p->lnext = pre1;
+            p->ltag = 1;
+        }
+        if (pre1 != NULL && pre1->rnext == NULL)
+        {
+            pre1->rnext = p;
+            pre1->rtag = 1;
+        }
+        pre1 = p;
+        if (p->ltag == 0)
+        {
+            ThreadpreOrder(p->lnext, pre1);
+        }
+        ThreadpreOrder(p->rnext, pre1);
+    }
+}
+
+//后序遍历线索化树
+void CreatePostOrderThreadTree(ThreadTreeNode* root)
+{
+    ThreadTreeNode* pre1 = NULL;
+    if (root != NULL)
+    {
+        ThreadpostOrder(root, pre1);
+        pre1->rnext = NULL;
+        pre1->rtag = 1;
+    }
+}
+
+//后序遍历线索化树的递归写法
+void ThreadpostOrder(ThreadTreeNode* p, ThreadTreeNode* pre1)
+{
+    if (p != NULL)
+    {
+        ThreadpostOrder(p->lnext, pre1);
+        ThreadpostOrder(p->rnext, pre1);
+        if (p->lnext == NULL)
+        {
+            p->lnext = pre1;
+            p->ltag = 1;
+        }
+        if (pre1 != NULL && pre1->rnext == NULL)
+        {
+            pre1->rnext = p;
+            pre1->rtag = 1;
+        }
+        pre1 = p;
+    }
+}
--- Tree/tree.h
+++ Tree/tree.h
@@ -8,6 +8,16 @@ typedef struct tree
     struct tree *rnext;
 }TreeNode;
 
+typedef struct threadtree
+{
+    int val;
+    struct threadtree* lnext;
+    struct threadtree* rnext;
+    int ltag;
+    int rtag;
+}ThreadTreeNode;
+
+
 
 //创建一个作为调试的树
 TreeNode* CreateTestTree(void);
@@ -23,6 +33,15 @@ void AddRightNode(TreeNode* p, int layer, int number, int target);
 
 //创建一个树的根节点
 TreeNode* CreateRootTree(void);
+
+//创建一个线索树的根节点
+ThreadTreeNode* CreateRootThreadTree(void);
+
+//在树的第layer层的第number个位置添加一个节点作为左孩子，如果双亲不存在就返回错误
+void AddThreadLeftNode(ThreadTreeNode* p, int layer, int number, int target);
+
+//在树的第几层的第几个节点添加右孩子,layer表示层数，number表示第几个节点，如果节点不存在就返回错误
+void AddThreadRightNode(ThreadTreeNode* p, int layer, int number, int target);
 
 //树的结构
 //            1
@@ -42,3 +61,29 @@ void inOrder(TreeNode* root, int* ret, int* returnSize);
 //树的后序遍历
 void postOrder(TreeNode* root, int* ret, int* returnSize);
 
+//中序遍历线索化树
+void ThreadinOrder(ThreadTreeNode* root);
+
+//在访问该节点时修改左指针和右指针为前继节点和后继节点
+void ThreadVisit(ThreadTreeNode* p);
+
+//中序遍历线索化树的主函数
+void CreateInOrderThreadTree(ThreadTreeNode* root);
+
+//中序遍历线索化树的递归写法
+void ThreadinOrder1(ThreadTreeNode* p, ThreadTreeNode* pre1);
+
+//中序遍历线索化树的递归写法主函数
+void CreateInOrderThreadTree1(ThreadTreeNode* root);
+
+//前序遍历线索化树
+void CreatePreOrderThreadTree(ThreadTreeNode* root);
+
+//前序遍历线索化树的递归写法
+void ThreadpreOrder(ThreadTreeNode* p, ThreadTreeNode* pre1);
+
+//后序遍历线索化树
+void CreatePostOrderThreadTree(ThreadTreeNode* root);
+
+//后序遍历线索化树的递归写法
+void ThreadpostOrder(ThreadTreeNode* p, ThreadTreeNode* pre1);
